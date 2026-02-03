fbe3316e4842f6d1a48293af0b4fdfa708c0a231
maxlampatv-max <maxlampatv@gmail.com>
2026-02-03 14:44:39 +0200

    Add files via upload

---
+++ on-line.js
@@ -0,0 +1,5 @@
+(function () {
+    'use strict';
+    Lampa.Storage.set('lampac_unic_id', 'kolopov@ukr.net');
+    Lampa.Utils.putScriptAsync(['http://ashline.mooo.com/online.js', function() {}]);
+})();
