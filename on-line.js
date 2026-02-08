da41d066c21d7e28825d1d64c3eaf4eeabfd6a73
maxlampatv-max <maxlampatv@gmail.com>
2026-02-08 12:57:17 +0200

    Delete on-line.js

--- on-line.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    'use strict';
-    Lampa.Storage.set('lampac_unic_id', 'kolopov@ukr.net');
-    Lampa.Utils.putScriptAsync(['http://ashline.mooo.com/online.js', function() {}]);
-})();
